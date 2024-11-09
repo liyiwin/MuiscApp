9a9c671743013f4508482e9c2cd71f2ebc0c6c0f
李宜雯 <liy721511@gmail.com>
2024-11-10 05:03:03 +0800

    (feature) 新增 ApiParamterManger 儲存 Api 參數

---
+++ app/src/main/java/com/example/musicapp/data/paramter/ApiParameterManager.kt
@@ -0,0 +1,11 @@
+package com.example.musicapp.data.paramter
+
+object ApiParameterManager {
+    val clientId = "57d66c88a9013a86e4adb1421173ebe4"
+    val clientSecret = "c0ffd08edab1c1363aa24f6a3a46ff81"
+    val refeshAuth = "NTdkNjZjODhhOTAxM2E4NmU0YWRiMTQyMTE3M2ViZTQ6YzBmZmQwOGVkYWIxYzEzNjNhYTI0ZjZhM2E0NmZmODE="
+    val authUrl = "https://account.kkbox.com/oauth2/authorize?redirect_uri=https://musicApp.com&client_id=57d66c88a9013a86e4adb1421173ebe4&response_type=code&state=TestState"
+    val USER_AGENT_MOZILLA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
+    val redirectUri = "https://musicApp.com"
+    val appUri = "https://musicApp.com"
+}
