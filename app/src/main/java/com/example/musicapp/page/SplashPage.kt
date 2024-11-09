17c49ea9b8f0b097cb1addb91de2fe84050307d3
李宜雯 <liy721511@gmail.com>
2024-11-10 04:34:35 +0800

    (feature) 新增啟動頁面

---
+++ app/src/main/java/com/example/musicapp/page/SplashPage.kt
@@ -0,0 +1,52 @@
+package com.example.musicapp.page
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.toColorInt
+import com.example.musicapp.R
+import com.example.musicapp.components.Timer
+import com.example.musicapp.routing.Screen
+import com.example.musicapp.theme.Theme
+
+
+@Composable
+fun SplashPage (navigationController:(screen: Screen)->Unit){
+    Theme {
+        mainContent()
+    }
+}
+
+@Composable
+private fun mainContent(){
+    Column(modifier = Modifier.fillMaxSize()
+    .background(
+        brush = Brush.verticalGradient(
+            startY = -500f,
+            colors = listOf(
+                Color("#00E3E3".toColorInt()),
+                Color("#FFD2D2".toColorInt()),
+            )
+        )
+    ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.splash_logo),
+            contentDescription = "",
+            modifier = Modifier.size(200.dp).fillMaxSize(),
+            alignment = Alignment.Center
+        )
+    }
+}
