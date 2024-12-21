f55af0db45d516d445c0232130d389982e69a79a
李宜雯 <liy721511@gmail.com>
2024-12-21 20:48:30 +0800

    (feature)  MusicApplication 依賴注入  PlayListManager 並 在 OnCreate 時 建立歌曲檔案總路徑

--- app/src/main/java/com/example/musicapp/application/MusicApplication.kt
+++ app/src/main/java/com/example/musicapp/application/MusicApplication.kt
@@ -1,7 +1,18 @@
 package com.example.musicapp.application
 
 import android.app.Application
+import com.example.musicapp.tool.file.IPlayListManager
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MusicApplication  : Application()
+class MusicApplication  : Application(){
+     @Inject
+     lateinit var playListManager: IPlayListManager
+
+    override fun onCreate() {
+        super.onCreate()
+        playListManager.createAppDirIfNotExist()
+    }
+
+}
