b59b9bd6e9bf1fc792500b68d0f3d22cd1c7607e
李宜雯 <liy721511@gmail.com>
2024-12-21 20:56:27 +0800

    (feature) MainActivity 新增 清除 App 資料的相關 function

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.musicapp
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.KeyEvent
+import android.webkit.CookieManager
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
@@ -13,19 +15,62 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.ExperimentalComposeUiApi
+import com.example.musicapp.localDatase.ISaveAppSettingInApp
+import com.example.musicapp.localDatase.ISaveUserInfoInApp
+import com.example.musicapp.localDatase.RouterDataStorage
+import com.example.musicapp.localDatase.sqLite.SQLiteCleaner
 import com.example.musicapp.tool.network.NetWorkSubject
 import com.example.musicapp.tool.network.Observer
 import com.example.musicapp.viewmodel.global.NetWorkViewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private var netWorkConnectionObserver: Observer<Boolean>? = null
     private var netWorkSubject: NetWorkSubject? = null
     private val netWorkViewModel: NetWorkViewModel by viewModels()
+    @Inject
+    lateinit var saveUserInfoInApp: ISaveUserInfoInApp
+    @Inject
+    lateinit var saveAppSettingInApp: ISaveAppSettingInApp
     private val externalPageNavigationController: (uri: Uri)->Unit  = {   uri ->
        this@MainActivity.startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+
+
+
+    private val cleanAppDataController:()->Unit = {
+        clearCache(this)
+        SQLiteCleaner.clearFavoriteTracksStorage(this)
+        saveUserInfoInApp.cleanData()
+        saveAppSettingInApp.cleanData()
+        RouterDataStorage.cleanData()
+        clearWebViewCookies()
+    }
+
+
+
+    private fun clearCache(context: Context) {
+        val cacheDir = context.cacheDir
+        deleteFilesInDir(cacheDir)  // 刪除緩存目錄中的所有檔案
+    }
+
+    private  fun deleteFilesInDir(dir: File) {
+        if (dir.isDirectory) {
+            val files = dir.listFiles()
+            files?.forEach { it.delete() }  // 刪除每個檔案
+        } else {
+            dir.delete()  // 刪除檔案
+        }
+    }
+
+    private  fun clearWebViewCookies() {
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.removeAllCookies(null)  // 清除所有 cookies
+        cookieManager.flush()  // 確保所有 cookies 已清除
     }
 
     @ExperimentalComposeUiApi
